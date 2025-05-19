7b4c042e2276fb77828509f05a4cc9eddac4daaa
BioElectronicsLab <bioelectronicslaboratory@gmail.com>
2025-05-19 16:16:14 +0300

    Add files via upload
    Updated welcome message

--- WelcomeMessage.m
+++ WelcomeMessage.m
@@ -3,7 +3,7 @@ clc;
 disp('  OO OO       NELM 1.0 beta by ImpeDance! Project')
 disp('  OO OO       Daniil Stupin, Natalia Boitsova, Anna Abelit')
 disp('OO     OO     Alferov University, Laboratory of BioElectronics');
-disp('OO OOO OO     2024');
+disp('OO OOO OO     2025');
 disp('  OOOOO     ');
 disp('  OOOOO       To cite use ''NELM software by Stupin, Boitsova, and Abelit''');
 disp('  OOOOO');
